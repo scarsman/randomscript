5e0866917025646ab3e95a08c6880f858cc659f3
Perpo Hipolito <poohsna@gmail.com>
2017-10-03 10:39:56 +0800

    Update mysqlrestarter.sh

--- mysqlrestarter.sh
+++ mysqlrestarter.sh
@@ -7,10 +7,11 @@
 
 timeToday=$(date)
 today=`date +%Y-%m-%d`
-mariadb_status=`systemctl status mariadb.service |  grep -q -w 'inactive\|failed' && echo 'inactive' || echo 'active'`
+service="mariadb"
+service_status=`systemctl status $service.service |  grep -q -w 'inactive\|failed' && echo 'inactive' || echo 'active'`
 
 
-if [ $mariadb_status == 'inactive' ]; then
+if [ $service_status == 'inactive' ]; then
 
 		if [ ! -e /tmp/attempt-$today ]; then
 			echo "0" > /tmp/attempt-$today
@@ -26,26 +27,26 @@ if [ $mariadb_status == 'inactive' ]; then
 		if [  $total -gt 5 ]; then
 			#send email
                         echo "Send last 30 lines of logs from file"
-                        tail -n 30 /tmp/mysql-$today.log > /tmp/sentToEmail.log
-                        echo -e "Mysql Status: $mariadb_status\nMore than 5 attempts to restart the mysql service.\n\nAdditional info: \n\n $(cat /tmp/sentToEmail.log)" > /tmp/sentToEmail.log
+                        tail -n 30 /tmp/$service-$today.log > /tmp/sentToEmail.log
+                        echo -e "$service status: $service_status\nMore than 5 attempts to restart the $service service.\n\nAdditional info: \n\n $(cat /tmp/sentToEmail.log)" > /tmp/sentToEmail.log
                         sendEmail -f "sender@gmail.com" -u "Alert: Mysql-Error from [$HOSTNAME] needs attention" -t "receiver@doamin.com" -cc="anotherreceiver@domain.com" -s "smtp.gmail.com:587" -o tls=yes -xu "sender@gmail.com" -xp "password" -o message-file="/tmp/sentToEmail.log" > /dev/null
                         
                         exit 1
 
 		fi 
 
-        echo "" >> /tmp/mysql-$today.log
-        echo "-----------------------------------------------------" >> /tmp/mysql-$today.log
-        echo "Date: $timeToday" >> /tmp/mysql-$today.log
-        echo "-----------------------------------------------------" >> /tmp/mysql-$today.log
-        echo " Mariadb was stop. Restarting the mariadb." >>/tmp/mysql-$today.log
-        echo "Last 30 lines of logs return from mariadb" >> /tmp/mysql-$today.log
-        echo "" >> /tmp/mysql-$today.log
-        echo "" >> /tmp/mysql-$today.log
-        journalctl -u mariadb | tail -n 30 >> /tmp/mysql-$today.log
-        systemctl start mariadb.service
-        mariadb_status_now=`systemctl status mariadb.service |  grep -q -w 'inactive\|failed' && echo 'inactive' || echo 'active'` 
-        echo "" >> /tmp/mysql-$today.log
-        echo "Mariadb status now: $mariadb_status_now" >> /tmp/mysql-$today.log
+        echo "" >> /tmp/$service-$today.log
+        echo "-----------------------------------------------------" >> /tmp/$service-$today.log
+        echo "Date: $timeToday" >> /tmp/$service-$today.log
+        echo "-----------------------------------------------------" >> /tmp/$service-$today.log
+        echo " Mariadb was stop. Restarting the mariadb." >>/tmp/$service-$today.log
+        echo "Last 30 lines of logs return from mariadb" >> /tmp/$service-$today.log
+        echo "" >> /tmp/$service-$today.log
+        echo "" >> /tmp/$service-$today.log
+        journalctl -u $service | tail -n 30 >> /tmp/$service-$today.log
+        systemctl start $service.service
+        service_status_now=`systemctl status $service.service |  grep -q -w 'inactive\|failed' && echo 'inactive' || echo 'active'` 
+        echo "" >> /tmp/$service-$today.log
+        echo "Mariadb status now: $service_status_now" >> /tmp/$service-$today.log
 
 fi
